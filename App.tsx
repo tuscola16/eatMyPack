55521038eb615ea67adaae30f6fc20dbb45361f8
MSI\shnmck <tuscola16@gmail.com>
2026-04-19 06:43:52 -0400

    adding sentry

--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Sentry from '@sentry/react-native';
 
-export default function App() {
+Sentry.init({
+  dsn: 'https://a4c49b45bafc53e557f70447d265b9be@o4511246237499392.ingest.us.sentry.io/4511246239989760',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+});
 
 const styles = StyleSheet.create({
   container: {
